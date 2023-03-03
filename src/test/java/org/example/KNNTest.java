ee93c2e9750779de7ce5ffa9960b5a7598ebbcad
Aleix <al394752@uji.es>
2023-03-03 12:35:23 +0100

    03/03

---
+++ src/test/java/org/example/KNNTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KNNTest {
+
+    @Test
+    void estimate() {
+        List<Double> data = new ArrayList<>();
+        data.add(4.6);
+        data.add(3.1);
+        data.add(1.5);
+        data.add(0.2);
+
+        CSV csv = new CSV();
+        String separator = System.getProperty( "file.separator" );
+        TableWithLabels table = csv.readTableWithLabels("src" + separator + "iris.csv");
+        KNN knn = new KNN();
+        knn.train(table);
+
+        Integer resultado = knn.estimate(data);
+        Integer esperado = table.labelsToIndex.get("Iris-setosa");
+
+        assertEquals(esperado, resultado);
+    }
+}
